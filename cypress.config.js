0a08ec29548458862fe04143c865cdd77c2802cf
Venkata Kishore Koneti <kkishore971@gmail.com>
2025-05-06 16:00:57 +0100

    Updated with BDD.

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,20 @@
 const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild");
+const addCucumberPlugin = require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    specPattern: "cypress/e2e/features/**/*.feature",
+    supportFile: false,
+    async setupNodeEvents(on, config) {
+      const bundler = createBundler({
+        plugins: [createEsbuildPlugin(config)],
+      });
+
+      on("file:preprocessor", bundler);
+      await addCucumberPlugin(on, config);
+      return config;
     },
   },
 });
---
+++ cypress/e2e/step_definitions/loginSteps.js
@@ -0,0 +1,15 @@
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+
+Given('I open the login page', () => {
+  cy.visit('/practice-test-login/');
+});
+
+When('I enter valid credentials', () => {
+  cy.get('#username').type('student');
+  cy.get('#password').type('Password123');
+  cy.get('#submit').click();
+});
+
+Then('I should see the success message', () => {
+  cy.contains('Logged In Successfully').should('be.visible');
+});
