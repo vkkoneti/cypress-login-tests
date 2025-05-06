8e45c575771a87a7248c8a28e17e6f738b4dfc65
Venkata Kishore Koneti <kkishore971@gmail.com>
2025-05-06 18:24:48 +0100

    Updated file structure and configurations to work with BDD.

--- cypress.config.js
+++ cypress.config.js
@@ -1,18 +1,21 @@
 const { defineConfig } = require("cypress");
 const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
-const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
-const addCucumberPreprocessorPlugin = require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
+const { createEsbuildPlugin } = require("@badeball/cypress-cucumber-preprocessor/esbuild");
+const addCucumberPreprocessorPlugin = require("@badeball/cypress-cucumber-preprocessor");
 
 module.exports = defineConfig({
   e2e: {
-    specPattern: "cypress/e2e/features/**/*.feature",
+    specPattern: "cypress/e2e/features/*.feature",
     async setupNodeEvents(on, config) {
       const bundler = createBundler({
         plugins: [createEsbuildPlugin(config)],
       });
 
       on("file:preprocessor", bundler);
-      await addCucumberPreprocessorPlugin(on, config);
+      await addCucumberPreprocessorPlugin.addCucumberPreprocessorPlugin(on, {
+        ...config,
+        stepDefinitions: "cypress/support/**/*.js",
+      });
 
       return config;
     },
