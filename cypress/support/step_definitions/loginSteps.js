b03afe439c4017f0e8bb02ae7c3157d88eb2e7b1
Venkata Kishore Koneti <kkishore971@gmail.com>
2025-07-01 18:54:49 +0100

    Add full login and practice page navigation test scenario

--- cypress/support/step_definitions/loginSteps.js
+++ cypress/support/step_definitions/loginSteps.js
@@ -1,5 +1,6 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
+// Login Flow
 Given("I open the login page", () => {
   cy.visit("https://practicetestautomation.com/practice-test-login/");
 });
@@ -12,4 +13,28 @@ When("I enter valid credentials", () => {
 
 Then("I should see the success message", () => {
   cy.contains("Logged In Successfully").should("be.visible");
+});
+
+// Practice Navigation
+When("I click on the Practice menu", () => {
+  cy.contains("Practice").click();
+});
+
+When("I click on the Test Login Page link", () => {
+  cy.contains("Test Login Page").click();
+});
+
+Then("I should see the login test instructions", () => {
+  cy.url().should("include", "practice-test-login");
+  cy.contains("Test login").should("be.visible");
+});
+
+When("I go back and click on the Test Exceptions link", () => {
+  cy.go("back");
+  cy.contains("Test Exceptions").click();
+});
+
+Then("I should see the exceptions test instructions", () => {
+  cy.url().should("include", "practice-test-exceptions");
+  cy.contains("Test Exceptions").should("be.visible");
 });
